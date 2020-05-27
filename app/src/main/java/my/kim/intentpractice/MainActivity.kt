42636e7eee1f151e93625b0e3ff651c3a9fbabc1
mykim76 <iyoungfm@gmail.com>
2020-05-27 21:42:15 +0900

    첫번째 엑티비티 소환

--- app/src/main/java/my/kim/intentpractice/MainActivity.kt
+++ app/src/main/java/my/kim/intentpractice/MainActivity.kt
@@ -1,12 +1,20 @@
 package my.kim.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goToFirstBtn.setOnClickListener {
+            val myIntent = Intent(this, FirstActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
