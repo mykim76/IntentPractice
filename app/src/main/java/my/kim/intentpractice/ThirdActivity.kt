68f0b52f633e86778087dc2712c9e5d7fc49ecd0
mykim76 <iyoungfm@gmail.com>
2020-05-27 22:16:29 +0900

    첨부된 내용 출력/ 보냈으나 사용하지 않아도 되는 코드 추가

--- app/src/main/java/my/kim/intentpractice/ThirdActivity.kt
+++ app/src/main/java/my/kim/intentpractice/ThirdActivity.kt
@@ -2,11 +2,16 @@ package my.kim.intentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val content = intent.getStringExtra("input")
+        contentTxt.text= content
+
     }
 }
