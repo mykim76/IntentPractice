a1a5b3295f1378a0cd808f111955ce70fc08357a
mykim76 <iyoungfm@gmail.com>
2020-05-27 22:03:31 +0900

    화면구조 설정 => 메인에서 입력한 내용을 세번째 엑티비티에서 출력

--- app/src/main/java/my/kim/intentpractice/SecondActivity.kt
+++ app/src/main/java/my/kim/intentpractice/SecondActivity.kt
@@ -10,7 +10,7 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        goBack2Btn.setOnClickListener {
+        goBackBtn.setOnClickListener {
             finish()
 
         }
