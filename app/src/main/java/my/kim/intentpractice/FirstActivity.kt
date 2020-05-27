5d529427b1d3c4bb6d1d89fe34bb4ddd42b6ef10
mykim76 <iyoungfm@gmail.com>
2020-05-27 21:48:53 +0900

    첫번째 엑티비티에서 돌아가기

--- app/src/main/java/my/kim/intentpractice/FirstActivity.kt
+++ app/src/main/java/my/kim/intentpractice/FirstActivity.kt
@@ -1,12 +1,22 @@
 package my.kim.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+
+       goBackBtn.setOnClickListener {
+           //돌아갈때는 intent로 돌아가지 않음 왜냐면 인텐트는 새로운 화면을 만드는 효과가 있기에
+//            val myIntent = Intent(this, MainActivity::class.java)
+           finish()
+
+        }
     }
 }
--- app/src/main/java/my/kim/intentpractice/MainActivity.kt
+++ app/src/main/java/my/kim/intentpractice/MainActivity.kt
@@ -12,9 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         goToFirstBtn.setOnClickListener {
+            //비행기 티켓 발권 (출발지와 / 도착지를 명시한다)
             val myIntent = Intent(this, FirstActivity::class.java)
+            // 실제 비행기 탑승 (출발)
+            // 특이사항 : 실제로는 액티비티 이동이 아니라 기존에 얹어
             startActivity(myIntent)
         }
+
+
 
     }
 }
